bf7c31f8bf62819620f873d753b147f3b1760216
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2022-10-14 16:29:16 +0100

    landing page continuation

--- components/Homepage/index.tsx
+++ components/Homepage/index.tsx
@@ -1,12 +1,28 @@
+import { GetServerSideProps } from "next";
 import React from "react";
 import Landing from "./Landing";
 
 const Index = () => {
   return (
-    <main className='relative h-[200vh] bg-[#E7ECEC]'>
-      <Landing />
-    </main>
+    <>
+      <main className='relative h-[200vh] bg-[#E7ECEC]'>
+        <Landing />
+      </main>
+      <section className='relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]'>
+        <div className='space-y-10 py-16'>
+          <h1 className='text-center text-4xl font-medium tracking-wide text-white md:text-5xl'>
+            New Promos
+          </h1>
+        </div>
+      </section>
+    </>
   );
 };
 
 export default Index;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  };
+};
