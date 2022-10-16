70fb99e5a75941eb63365d7d310bf0f1d7858d35
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2022-10-16 23:37:07 +0100

    added react hot toast for notification

--- components/Homepage/SelectedProduct.tsx
+++ components/Homepage/SelectedProduct.tsx
@@ -4,16 +4,19 @@ import { urlFor } from "../../sanity";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useAppDispatch } from "../../redux/hooks";
 import { addToCart } from "../../redux/cartSlice";
+import toast, { Toaster } from "react-hot-toast";
 
 const SelectedProduct = ({ activeProducts }: ActiveProductsProps) => {
   const dispatch = useAppDispatch();
 
   const handleAddToCart = (item: ProductsProps) => {
     dispatch(addToCart(item));
+    toast.success(`${item.title} was added to your cart`);
   };
 
   return (
     <div className='grid grid-cols-1 gap-4 px-8 text-white transition-all duration-300 ease-linear sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
+      <Toaster />
       {activeProducts.map((product: ProductsProps) => (
         <div
           key={product._id}
